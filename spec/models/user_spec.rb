61c6b5a96166d069bb63176a3b2d74454c64b4bf
Kacper <tarchala.k@gmail.com>
2015-12-14 14:29:44 +0100

    Test - Feature -logi via pop-up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,23 +2,48 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it 'should create valid admin user' do
-    admin = create(:admin)
+  describe 'create' do
+    it 'valid admin' do
+      admin = create(:admin)
+      expect(admin.email).to eq('admin@admin.com')
+      expect(admin.first_name).to eq('Admin')
+      expect(admin.last_name).to eq('Doel')
+      expect(admin.admin).to eq(true)
+    end
 
-    expect(admin.email).to eq('admin@admin.com')
-    expect(admin.first_name).to eq('Admin')
-    expect(admin.last_name).to eq('Doel')
-    expect(admin.admin).to eq(true)
+    it 'valid normal' do
+      user = create(:user)
+      expect(user.email).to eq('john@gmail.com')
+      expect(user.first_name).to eq('John')
+      expect(user.last_name).to eq('Doel')
+      expect(user.admin).to eq(false)
+    end
   end
 
-  it 'should create valid normal user' do
+
+  describe 'update' do
+    it 'normal to admin' do
+      user = create(:user)
+      user.update(admin: true)
+      expect(user.admin).to eq(true)
+    end
+
+    it 'last_name' do
+      user = create(:user)
+      user.update(last_name: 'Toe')
+      expect(user.last_name).to eq('Toe')
+    end
+
+    it 'first_name' do
+      user = create(:user)
+      user.update(first_name: 'Michael')
+      expect(user.first_name).to eq('Michael')
+    end
+  end
+
+  it 'should be destroy' do
     user = create(:user)
-
-    expect(user.email).to eq('john@gmail.com')
-    expect(user.first_name).to eq('John')
-    expect(user.last_name).to eq('Doel')
-    expect(user.admin).to eq(false)
+    user.destroy
+    expect(User.all.size).to eq(0)
   end
-
-
 end
