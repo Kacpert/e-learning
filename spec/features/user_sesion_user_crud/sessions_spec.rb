372080076c8e447fa7ddf36b8cd31e385f3c035a
Tomasz Skorupka <tomaszskorupka94@gmail.com>
2015-12-14 16:50:46 +0100

    Added seeds for admin user. Edit user added in nav. Changed buttons to links in tests.

--- spec/features/user_sesion_user_crud/sessions_spec.rb
+++ spec/features/user_sesion_user_crud/sessions_spec.rb
@@ -4,10 +4,10 @@ RSpec.feature "Sessions", type: :feature do
   it 'should login user' do
     user = create(:user)
     visit '/'
-    click_button 'Sign in'
+    click_link 'Sign in'
     fill_in 'Email', :with => user.email
     fill_in 'Password', :with => user.password
     find('input[type="submit"][name="commit"][value="Sign in"]').click
-    expect(page.has_button?('Sign out')).to eq(true)
+    expect(page.has_link?('Sign out')).to eq(true)
   end
 end
