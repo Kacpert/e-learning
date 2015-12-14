45e94085b10628403b67ba1dcd5484de8a4e4659
Kacper <tarchala.k@gmail.com>
2015-12-14 15:56:59 +0100

    Test - fix ficture customer spec

--- spec/features/user_sesion_user_crud/customer_spec.rb
+++ spec/features/user_sesion_user_crud/customer_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.feature "Customers", type: :feature do
   it 'should be able to edit they own account' do
     user = create(:user)
+    sign_in user
     visit '/'
-    click_button 'Edit account'
+    first(:button, 'Edit account').click 
     fill_in 'First name', with: 'Tomas'
     fill_in 'Last name', with: 'Richy'
     fill_in 'Current password', with: user.password
-    click_button 'Update Account'
+    first(:button, 'Update Account').click 
     expect(page.has_button?('Log out')).to eq(true)
   end
 end
