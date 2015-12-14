3bc1162eed134469f651424688ee4494798a0363
Kacper <tarchala.k@gmail.com>
2015-12-14 13:49:21 +0100

    Test - factories - fix user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,20 @@
 FactoryGirl.define do
   factory :user do
-    email       'john@gmail.com'
-    first_name  'John'
-    last_name   'Doe'
-    admin       false
+    email                   'john@gmail.com'
+    first_name              'John'
+    last_name               'Doe'
+    admin                   false
+    password                'password'
+    password_confirmation   'password'
   end
 
   # This will use the User class (Admin would have been guessed)
   factory :admin, class: User do
-    email       'admin@admin.com'
-    first_name  'Admin'
-    last_name   'Doel'
-    admin       false
+    email                   'admin@admin.com'
+    first_name              'Admin'
+    last_name               'Doel'
+    admin                   false
+    password                'password'
+    password_confirmation   'password'
   end
 end
