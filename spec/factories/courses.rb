9f340ba2dedff59031d4f56fdf069dd47b26e09e
Kacper <tarchala.k@gmail.com>
2015-12-15 16:35:27 +0100

    Test - courses - add image

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :course do
     name          'New course'
     description   'some desc'
+    image_file_name       { 'lesson_image.jpg' }
+    image_content_type    { 'image/png' }
+    image_file_size       { 1024 }
   end
 end
