823af04d0b2539e26247b84217308f3232cdb89d
Kacper <tarchala.k@gmail.com>
2015-12-17 15:28:44 +0100

    Tets - API - courses

--- spec/requests/api/v1/courses_spec.rb
+++ spec/requests/api/v1/courses_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe "Courses api", :type => :request do
       expect(json['course']['image_url']).to match(/http|https/)
     end
   end
+
+  it 'should return list of courses with properly image link' do
+    courses = FactoryGirl.create_list(:course, 15)
+    get "/api/v1/courses"
+
+    # test for the 200 status-code
+    expect(response).to be_success
+    expect(json['courses'].length).to eq(15)
+    expect(json['courses']['0']['name']).to eq('New course')
+    expect(json['courses']['0']['image_url']).to match(/http|https/)
+
+  end
 end
