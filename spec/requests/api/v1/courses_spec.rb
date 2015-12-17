70617a2dcfa2890a19e0aa959d6d133171e77649
Kacper <tarchala.k@gmail.com>
2015-12-17 12:22:55 +0100

    hot fix

--- spec/requests/api/v1/courses_spec.rb
+++ spec/requests/api/v1/courses_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe "Course api", :type => :request do
   describe "Course" do
     it 'sends a specyfic course' do
       course = FactoryGirl.create(:course)    
-      get "/api/v1/messages/#{message.id}"
+      get "/api/v1/courses/#{message.id}"
 
       # test for the 200 status-code
       expect(response).to be_success
     end
   end
 
-end
+
