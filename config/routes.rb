b11932bcd6129df4c43177f057aadad13ab1430d
Kacper <tarchala.k@gmail.com>
2016-01-21 12:30:23 +0100

    Api - create user

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :users, :only => [:show]
+      resources :users, only: [:show, :create]
       resources :categories, only: [:show, :index]
       resources :courses, only: [:show, :index] do
         resources :lessons, only: [:index]
