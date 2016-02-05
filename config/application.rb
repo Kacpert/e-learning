ba23d9465bb73d313f40069653d03d1de79fcde0
Tomasz Skorupka <tomaszskorupka@Kasias-MacBook-Pro.local>
2016-02-05 14:43:28 +0100

    Reenabled cors support for ajax.

--- config/application.rb
+++ config/application.rb
@@ -39,11 +39,11 @@ module ELearning
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # config.middleware.insert_before 0, "Rack::Cors" do
-    #   allow do
-    #     origins '*'
-    #     resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
-    #   end
-    # end
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
