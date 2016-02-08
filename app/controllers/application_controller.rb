f9941ff25f23a3dc6a98a6430b08f21a6c753f09
Kacper <tarchala.k@gmail.com>
2016-02-08 11:43:27 +0100

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  skip_before_filter :verify_authenticity_token
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  # # Prevent CSRF attacks by raising an exception.
+  # # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
 
   def home
   end
