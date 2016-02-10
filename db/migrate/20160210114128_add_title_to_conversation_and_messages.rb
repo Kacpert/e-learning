2bdb7e45a052590ff5dc4d679966025da675bb50
Kacper <tarchala.k@gmail.com>
2016-02-10 13:24:05 +0100

    fix

--- db/migrate/20160210114128_add_title_to_conversation_and_messages.rb
+++ db/migrate/20160210114128_add_title_to_conversation_and_messages.rb
@@ -1,6 +1,6 @@
 class AddTitleToConversationAndMessages < ActiveRecord::Migration
   def change
-    add_column :conversations, :title, :strig, default: ""
-    add_column :messages, :title, :strig, default: ""
+    add_column :conversations, :title, :string, default: ""
+    add_column :messages, :title, :string, default: ""
   end
 end
