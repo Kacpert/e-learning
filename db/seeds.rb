5ae630a6ac135ed5b6b329ca38017bd99cd6c4e2
Kacper <tarchala.k@gmail.com>
2016-02-12 15:33:44 +0100

    heroku seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,24 @@
 
 SortingCourse.create(name: 'Your courses', user_dependency: true)
 SortingCourse.create(name: 'Most REcent', time_dependency: true)
+
+#messages
+u_1 = User.last(3)
+u_2 = User.last(4)
+u_3 = User.last(9)
+c_1 = Convesation.create(title: Faker::Name.name)
+c_2 = Convesation.create(title: Faker::Name.name)
+c_3 = Convesation.create(title: Faker::Name.name)
+c_1 << u_1
+c_2 << u_2
+c_3 << u_3
+create_messages(u_1, c_1)
+create_messages(u_2, c_2)
+create_messages(u_3, c_3)
+def create_messages(users, conversation)
+  10.times do
+    users.each do |user|
+      conversation.messages.create(user: user, title: Faker::Name.name, text: Faker::Lorem.sentence )
+    end
+  end
+end
